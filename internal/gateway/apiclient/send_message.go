83c0fef5f9790b2db566124fa0fa7224ef25882a
KentaOKada <kokada@sonix.asia>
2025-01-16 18:20:38 +0900

    line replace

--- internal/gateway/apiclient/send_message.go
+++ internal/gateway/apiclient/send_message.go
@@ -1,18 +1,27 @@
 package apiclient
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/url"
-	"strings"
 
 	"github.com/labstack/echo/v4"
 )
+
+type Message struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
 
 func (r Repository) SendMessage(ctx echo.Context, message string) error {
 	// アクセストークンはここに記載
 	accessToken := ""
 
-	URL := "https://notify-api.line.me/api/notify"
+	// UserIDはここに記載
+	userID := ""
+
+	URL := "https://api.line.me/v2/bot/message/push"
 
 	u, err := url.ParseRequestURI(URL)
 	if err != nil {
@@ -21,17 +30,20 @@ func (r Repository) SendMessage(ctx echo.Context, message string) error {
 
 	c := &http.Client{}
 
-	form := url.Values{}
-	form.Add("message", message)
-
-	body := strings.NewReader(form.Encode())
-
-	req, err := http.NewRequest("POST", u.String(), body)
+	msg := Message{Type: "text", Text: message}
+	b, err := json.Marshal(msg)
 	if err != nil {
 		return err
 	}
 
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	jsonStr := `{"to":"` + userID + `",` + `"messages":[` + string(b) + `]}`
+
+	req, err := http.NewRequest("POST", u.String(), bytes.NewBuffer([]byte(jsonStr)))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "Bearer "+accessToken)
 
 	_, err = c.Do(req)
